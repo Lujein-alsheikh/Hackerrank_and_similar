f2cec4ff2172cbe02c5b8264a4e69ab2c0067e8c
Lujein <lujein.alsheikh@gmail.com>
2024-02-20 21:09:33 +0100

    add AI problems

---
+++ Hackerrank/AI_problems/Bot_saves_princess.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+# other solutions on https://codereview.stackexchange.com/questions/131592/hackerrank-bot-saves-princess-beginner-code
+
+def displayPathtoPrincess(n,grid):
+#print all the moves here
+    mid_square = n-1/2
+    if grid[0][0]=="p":
+        first_move = "UP"
+        second_move = "LEFT"
+        
+    if grid[0][n-1] == "p":      
+        first_move="UP"
+        second_move="RIGHT"
+                 
+    if grid[n-1][0] == "p":
+        first_move="DOWN"
+        second_move="LEFT"
+    
+    if grid[n-1][n-1] == "p":
+        first_move="DOWN"
+        second_move="RIGHT"
+        
+    for _ in range(mid_square+1):
+        print(first_move)
+        print(second_move)
+      
+
+m = int(input())
+grid = [] 
+for i in range(0, m): 
+    grid.append(input().strip())  
+
+displayPathtoPrincess(m,grid)
