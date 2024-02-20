7109c4962eb80f0ddda46123babccb763dfd668c
Lujein <lujein.alsheikh@gmail.com>
2024-02-20 19:32:15 +0100

    Remove duplicated python folder

--- Hackerrank/Python_easy/shape_and_reshape.py
+++
@@ -1,9 +0,0 @@
-import numpy
-
-input_line = input()
-str_numbers = input_line.split()
-# numbers are entered as strings; we need to convert them to integers
-L = list(map(int, str_numbers))
-arr = numpy.array(L)
-# print(arr)
-print(numpy.reshape(arr,(3,3)))
